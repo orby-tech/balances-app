ea4a92456f5ea7394b1959513e671157bc0b25ab
Timur Bondarenko <tibode495@gmail.com>
2023-04-03 12:16:53 +0600

    add logout handler

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,3 @@ scp -r ./frontend/dist/* root@31.192.235.64:./manager/frontend/dist/
 scp -r ./backend/dist/* root@31.192.235.64:./manager/backend/dist/
 scp -r ./backend/package.json root@31.192.235.64:./manager/backend/package.json
 scp -r ./backend/src/* root@31.192.235.64:./manager/backend/src/
-scp ./backend/views/index.hbs root@31.192.235.64:./manager/backend/views/index.hbs
