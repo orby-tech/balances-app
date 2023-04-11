5028471aec89a912e278628e63282dfe9ddcd323
Timur Bondarenko <tibode495@gmail.com>
2023-04-11 22:03:04 +0600

    move logout button and add resizing left menu

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CurrenciesService } from './graphql/currencies.service';
+import { UserService } from './graphql/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
-  menuVisible = false;
+  menuVisible = window.innerWidth > 600;
+
+  user$ = this.userService.user$;
+  userMainCurrency$ = this.userService.userMainCurrency$;
+
+  constructor(
+    private userService: UserService,
+    private currenciesService: CurrenciesService
+  ) {
+    this.userService.load();
+    this.currenciesService.load();
+
+    const observer = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        this.menuVisible = entry.contentRect.width > 600;
+      });
+    });
+    observer.observe(document.body);
+  }
 
   logout() {
     localStorage.removeItem('token');
