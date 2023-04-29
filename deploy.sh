2e8a386e353c9ebe49f8d2fb961ec5edad763dca
Timur Bondarenko <tibode495@gmail.com>
2023-04-30 00:16:23 +0600

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,2 @@
 scp -r docker-compose.yml root@31.192.235.64:./manager/docker-compose.yml
 scp -r ./container/Dockerfile root@31.192.235.64:./manager/container/Dockerfile
-scp -r ./frontend/dist/* root@31.192.235.64:./manager/frontend/dist/
-scp -r ./backend/dist/* root@31.192.235.64:./manager/backend/dist/
-scp -r ./backend/package.json root@31.192.235.64:./manager/backend/package.json
-scp -r ./backend/src/* root@31.192.235.64:./manager/backend/src/
