6f6edd6e44449591c96378688fa9306196606560
Timur Bondarenko <tibode495@gmail.com>
2023-05-10 20:31:11 +0600

    start making currencies from service

--- backend/src/currencies/currencies.service.ts
+++ backend/src/currencies/currencies.service.ts
@@ -1,10 +1,11 @@
-import { CurrenciesRate } from '@common/graphql';
+import { CurrenciesRate, Currency } from '@common/graphql';
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CurrenciesService {
   ratesHistory: CurrenciesRate[] = [];
+  currencies: Currency[] = [];
 
   constructor(private readonly httpService: HttpService) {
     this.httpService
@@ -14,6 +15,18 @@ export class CurrenciesService {
       .subscribe((x) => {
         this.ratesHistory = x.data;
       });
+
+    this.httpService
+      .get<Currency[]>('http://currencies.orby-tech.space/api/currencies')
+      .subscribe((x) => {
+        this.currencies = x.data;
+        console.log('this.currencies', this.currencies);
+      });
+  }
+
+  async getCurrencies(): Promise<Currency[]> {
+    console.log('getCurrencies', this.currencies);
+    return this.currencies;
   }
 
   async getLatest(): Promise<CurrenciesRate[]> {
